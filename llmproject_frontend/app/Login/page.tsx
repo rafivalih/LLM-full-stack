60b8f51e738a12f0ec89ba4f094f30d98b2687b5
rafivalih <rafivalihshaik@gmail.com>
2026-09-08 18:55:08 +0530

    updated frontend

--- llmproject_frontend/app/Login/page.tsx
+++ llmproject_frontend/app/Login/page.tsx
@@ -4,8 +4,10 @@ const login = () => {
   return (
     <div className='bg-white min-h-[calc(100vh-60px)] text-black'>
 
-        <div className="">
-        
+        <div className=" flex flex-col gap-2">
+        <input placeholder="UserName" type="text" required className='rounder-md bg-white border-gray-400 border hover:border-gray-700 hover:scale-105' />
+        <input placeholder="mail@gmail.com" type="email"  required  className='rounder-md bg-white border-gray-400 border hover:border-gray-700 hover:scale-105'/>
+        <input placeholder="UserName" type="password" required  className='rounder-md bg-white border-gray-400 border hover:border-gray-700 hover:scale-105' />
         </div>
         
         </div>
